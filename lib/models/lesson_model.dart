2b40b4f32acb0183b7e0f1042374535e8578d75d
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:46:03 +0530

    Update lesson_model.dart

--- lib/models/lesson_model.dart
+++ lib/models/lesson_model.dart
@@ -1,1 +1,20 @@
+class LessonModel {
+  final int id;
+  final int courseId;
+  final String title;
+  final String videoUrl;
+  final String? notesUrl;
+  final int orderIndex;
 
+  LessonModel({required this.id, required this.courseId, required this.title,
+    required this.videoUrl, this.notesUrl, required this.orderIndex});
+
+  factory LessonModel.fromMap(Map<String, dynamic> map) => LessonModel(
+    id: map['id'],
+    courseId: map['course_id'],
+    title: map['title'],
+    videoUrl: map['video_url'],
+    notesUrl: map['notes_url'],
+    orderIndex: map['order_index'] ?? 0,
+  );
+}
