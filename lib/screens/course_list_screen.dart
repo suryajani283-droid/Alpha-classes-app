5ccb9c9372cafa2efdd88308907c6647c2fb5966
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:52:50 +0530

    Update course_list_screen.dart

--- lib/screens/course_list_screen.dart
+++ lib/screens/course_list_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/course_provider.dart';
+import '../widgets/course_card.dart';
 
+class CourseListScreen extends StatelessWidget {
+  const CourseListScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<CourseProvider>();
+    if (provider.loading) return const Center(child: CircularProgressIndicator());
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: provider.allCourses.length,
+      itemBuilder: (_, i) => CourseCard(course: provider.allCourses[i]),
+    );
+  }
+}
