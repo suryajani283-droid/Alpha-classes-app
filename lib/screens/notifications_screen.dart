883b8692bf7010f4efb67cbc65d0baab4e073126
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:55:41 +0530

    Update notifications_screen.dart

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/database_service.dart';
+import '../models/notification_model.dart';
 
+class NotificationsScreen extends StatefulWidget {
+  const NotificationsScreen({super.key});
+  @override
+  State<NotificationsScreen> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  late Future<List<NotificationModel>> _future;
+  @override
+  void initState() {
+    super.initState();
+    _future = DatabaseService().getNotifications();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notifications')),
+      body: FutureBuilder<List<NotificationModel>>(
+        future: _future,
+        builder: (_, snap) {
+          if (!snap.hasData) return const Center(child: CircularProgressIndicator());
+          return ListView.builder(
+            itemCount: snap.data!.length,
+            itemBuilder: (_, i) => ListTile(
+              title: Text(snap.data![i].title),
+              subtitle: Text(snap.data![i].message),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
