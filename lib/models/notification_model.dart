872443043f3fdf3057ae65c4a00e5934b45ae4f7
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:46:47 +0530

    Update notification_model.dart

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -1,1 +1,15 @@
+class NotificationModel {
+  final int id;
+  final String title;
+  final String message;
+  final DateTime createdAt;
 
+  NotificationModel({required this.id, required this.title, required this.message, required this.createdAt});
+
+  factory NotificationModel.fromMap(Map<String, dynamic> map) => NotificationModel(
+    id: map['id'],
+    title: map['title'],
+    message: map['message'],
+    createdAt: DateTime.parse(map['created_at']),
+  );
+}
