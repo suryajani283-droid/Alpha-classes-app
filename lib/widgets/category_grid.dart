cce3b362e3c7bfb6d16d809a29d18d23ee56c309
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:52:18 +0530

    Update category_grid.dart

--- lib/widgets/category_grid.dart
+++ lib/widgets/category_grid.dart
@@ -1,1 +1,16 @@
-
+import 'package:flutter/material.dart';
+class CategoryGrid extends StatelessWidget {
+  const CategoryGrid({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final categories = ['Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10', 'Class 11', 'Class 12', 'B.Sc'];
+    return GridView.builder(
+      shrinkWrap: true, physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, childAspectRatio: 1),
+      itemCount: categories.length,
+      itemBuilder: (_, i) => Card(
+        child: Center(child: Text(categories[i], textAlign: TextAlign.center)),
+      ),
+    );
+  }
+}
