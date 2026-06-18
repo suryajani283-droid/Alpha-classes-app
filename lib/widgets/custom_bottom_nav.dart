47e1f3dfb1a867ba36d60ca8815372f77d1877d4
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:50:53 +0530

    Update custom_bottom_nav.dart

--- lib/widgets/custom_bottom_nav.dart
+++ lib/widgets/custom_bottom_nav.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+  const CustomBottomNav({super.key, required this.currentIndex, required this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.indigo,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: 'Courses'),
+        BottomNavigationBarItem(icon: Icon(Icons.quiz), label: 'Tests'),
+        BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Store'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
