b4461ecacea3cb9ee4ea2a6c6d9f6fa44e8b5681
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:45:47 +0530

    Update course_model.dart

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -1,1 +1,24 @@
+class CourseModel {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String thumbnail;
+  final String teacher;
+  final String category;
+  bool isPurchased; // mutable for UI
 
+  CourseModel({required this.id, required this.title, required this.description,
+    required this.price, required this.thumbnail, required this.teacher,
+    required this.category, this.isPurchased = false});
+
+  factory CourseModel.fromMap(Map<String, dynamic> map) => CourseModel(
+    id: map['id'],
+    title: map['title'],
+    description: map['description'] ?? '',
+    price: (map['price'] ?? 0).toDouble(),
+    thumbnail: map['thumbnail'] ?? '',
+    teacher: map['teacher'] ?? '',
+    category: map['category'] ?? '',
+  );
+}
