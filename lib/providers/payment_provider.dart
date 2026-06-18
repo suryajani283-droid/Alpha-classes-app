c8426870b4e22c0c9dc6b6968dc55bf791d89fc5
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:48:23 +0530

    Update payment_provider.dart

--- lib/providers/payment_provider.dart
+++ lib/providers/payment_provider.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import '../services/payment_service.dart';
 
+class PaymentProvider extends ChangeNotifier {
+  final PaymentService _payment = PaymentService();
+  String? _paymentId;
+  bool _paymentSuccess = false;
+  bool _processing = false;
+
+  String? get paymentId => _paymentId;
+  bool get paymentSuccess => _paymentSuccess;
+  bool get processing => _processing;
+
+  void initiatePayment(double amount, String courseName, Function(String) onSuccess) {
+    _processing = true;
+    notifyListeners();
+    _payment.onSuccess = (id) {
+      _paymentId = id;
+      _paymentSuccess = true;
+      _processing = false;
+      onSuccess(id);
+      notifyListeners();
+    };
+    _payment.onFailure = () {
+      _paymentSuccess = false;
+      _processing = false;
+      notifyListeners();
+    };
+    _payment.openCheckout(amount, courseName);
+  }
+}
