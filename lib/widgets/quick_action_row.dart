eb44e0bec0b2ba9d26e56fb819ca576b583824cd
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:52:31 +0530

    Update quick_action_row.dart

--- lib/widgets/quick_action_row.dart
+++ lib/widgets/quick_action_row.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+class QuickActionRow extends StatelessWidget {
+  const QuickActionRow({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      _ActionChip(icon: Icons.live_tv, label: 'Live'),
+      _ActionChip(icon: Icons.ondemand_video, label: 'Recorded'),
+      _ActionChip(icon: Icons.assignment, label: 'Tests'),
+      _ActionChip(icon: Icons.download, label: 'Notes'),
+    ]);
+  }
+}
 
+class _ActionChip extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  const _ActionChip({required this.icon, required this.label});
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Icon(icon, color: Colors.indigo),
+      Text(label),
+    ]);
+  }
+}
