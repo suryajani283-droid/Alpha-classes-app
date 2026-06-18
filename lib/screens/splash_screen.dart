ed0659fb4ee8e1b0e1aed6f7c8b69d38c895850f
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:49:29 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,38 @@
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
+import 'providers/course_provider.dart';
+import 'providers/test_provider.dart';
+import 'providers/payment_provider.dart';
+import 'routes.dart';
+import 'services/supabase_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SupabaseService().initialize();
+  runApp(const AlphaClassesApp());
+}
+
+class AlphaClassesApp extends StatelessWidget {
+  const AlphaClassesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()..init()),
+        ChangeNotifierProvider(create: (_) => CourseProvider()),
+        ChangeNotifierProvider(create: (_) => TestProvider()),
+        ChangeNotifierProvider(create: (_) => PaymentProvider()),
+      ],
+      child: MaterialApp(
+        title: 'ALPHA CLASSES',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.indigo, fontFamily: 'Poppins'),
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: AppRoutes.generateRoute,
+      ),
+    );
+  }
+}
