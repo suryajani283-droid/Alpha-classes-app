220092913c4f3f63cd973857d181d3365bfff8e6
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:46:17 +0530

    Update test_model.dart

--- lib/models/test_model.dart
+++ lib/models/test_model.dart
@@ -1,1 +1,18 @@
+class TestModel {
+  final int id;
+  final int courseId;
+  final String title;
+  final int durationMinutes;
+  final bool isFree;
 
+  TestModel({required this.id, required this.courseId, required this.title,
+    required this.durationMinutes, required this.isFree});
+
+  factory TestModel.fromMap(Map<String, dynamic> map) => TestModel(
+    id: map['id'],
+    courseId: map['course_id'],
+    title: map['title'],
+    durationMinutes: map['duration_minutes'],
+    isFree: map['is_free'] ?? false,
+  );
+}
