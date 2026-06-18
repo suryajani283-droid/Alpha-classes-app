df4e1e9c83b8e490e5035e848bde2081564ab6f3
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:45:02 +0530

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,1 +1,7 @@
-
+class AppConfig {
+  static const supabaseUrl = 'YOUR_SUPABASE_URL';
+  static const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const razorpayKey = 'YOUR_RAZORPAY_KEY_ID';
+  static const appName = 'ALPHA CLASSES';
+  static const tagline = 'Mathematics Excellence';
+}
