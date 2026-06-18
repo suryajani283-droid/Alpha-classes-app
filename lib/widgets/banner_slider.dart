c48d7b2630b8245a330ccffa7b155e5c4454b01d
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:52:01 +0530

    Update banner_slider.dart

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -1,1 +1,17 @@
-
+import 'package:flutter/material.dart';
+class BannerSlider extends StatelessWidget {
+  const BannerSlider({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: PageView(
+        children: [
+          Container(color: Colors.indigo.shade100, child: const Center(child: Text('New Batches Starting Soon!'))),
+          Container(color: Colors.orange.shade100, child: const Center(child: Text('Scholarship Test on 25th June'))),
+          Container(color: Colors.green.shade100, child: const Center(child: Text('Download Free PYQ Papers'))),
+        ],
+      ),
+    );
+  }
+}
