b1e43bad047ca31816aa75661e1cff65fe67260c
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:45:30 +0530

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,26 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String? email;
+  final String? mobile;
+  final String? userClass;
+  final String? school;
+  final String? city;
+  final String? photoUrl;
+  final String role;
 
+  UserModel({required this.id, required this.name, this.email, this.mobile,
+    this.userClass, this.school, this.city, this.photoUrl, this.role = 'student'});
+
+  factory UserModel.fromMap(Map<String, dynamic> map) => UserModel(
+    id: map['id'],
+    name: map['name'] ?? '',
+    email: map['email'],
+    mobile: map['mobile'],
+    userClass: map['class'],
+    school: map['school'],
+    city: map['city'],
+    photoUrl: map['photo_url'],
+    role: map['role'] ?? 'student',
+  );
+}
