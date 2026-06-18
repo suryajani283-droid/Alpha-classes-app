7be46b68a75d4bf4d9e29174d1728448954c581a
suryajani283-droid <suryajani283@gmail.com>
2026-06-18 17:54:06 +0530

    Update test_screen.dart

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/test_provider.dart';
+import 'test_taking_screen.dart';
 
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<TestProvider>();
+    if (provider.loading) return const Center(child: CircularProgressIndicator());
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: provider.tests.length,
+      itemBuilder: (_, i) => ListTile(
+        title: Text(provider.tests[i].title),
+        subtitle: Text('${provider.tests[i].durationMinutes} mins • ${provider.tests[i].isFree ? "Free" : "Paid"}'),
+        trailing: const Icon(Icons.arrow_forward),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) => TestTakingScreen(testId: provider.tests[i].id, duration: provider.tests[i].durationMinutes)));
+        },
+      ),
+    );
+  }
+}
